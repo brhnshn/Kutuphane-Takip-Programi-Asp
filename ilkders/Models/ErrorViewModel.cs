a8d97cae0a4126efdcd4a3316ea35722935b9b10
brhnshn <sahinburhan501@gmail.com>
2025-11-24 00:11:29 +0300

    Add project files.

---
+++ ilkders/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ilkders.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
